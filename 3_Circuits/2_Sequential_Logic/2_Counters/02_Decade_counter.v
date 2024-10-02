ac3ba81bd61284fad487a64c6b28848b2376d27f
Tean Lai <laitean19@gmail.com>
2024-10-02 18:55:51 -0400

    implement some counter solutions

---
+++ 3_Circuits/2_Sequential_Logic/2_Counters/02_Decade_counter.v
@@ -0,0 +1,11 @@
+module top_module (
+    input clk,
+    input reset,        // Synchronous active-high reset
+    output [3:0] q);
+
+    always @(posedge clk) begin
+        if (reset || q >= 9) q <= 0;
+        else if (q < 9) q <= q + 1;
+    end
+
+endmodule
